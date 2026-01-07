3bf30a543382d4cc57365092fdce4a3f5fb98d78
Arnav <arnavsalouja@gmail.com>
2026-01-07 16:10:19 -0500

    Remove padding between nav and main content in MarketingLayout

--- components/layouts/MarketingLayout.tsx
+++ components/layouts/MarketingLayout.tsx
@@ -87,7 +87,7 @@ export default function MarketingLayout({ children }: MarketingLayoutProps) {
       </nav>
 
       {/* Main Content */}
-      <main className="pt-16">{children}</main>
+      <main>{children}</main>
     </div>
   )
 }
